e312f1b8c83f11c1f5cdd62cdba832ef2936c38e
Kaua Sousa <kauasantos2014s10@gmail.com>
2025-10-24 22:47:28 -0300

    Commit Inicial: Spring Boot Delivery API

---
+++ src/main/java/com/delivery_api/Projeto/de/Delivery/API/ProjetoDeDeliveryApiApplication.java
@@ -0,0 +1,13 @@
+package com.delivery_api.Projeto.de.Delivery.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoDeDeliveryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoDeDeliveryApiApplication.class, args);
+	}
+
+}
