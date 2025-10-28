ec40ecbe913e7b39c38eb81b61d2683730a92eda
Kaua Sousa <kauasantos2014s10@gmail.com>
2025-10-27 21:32:51 -0300

    Banco de dados H2: remoção de comentário em properties

--- src/main/java/com/delivery_api/Projeto/de/Delivery/API/entity/Cliente.java
+++ src/main/java/com/delivery_api/Projeto/de/Delivery/API/entity/Cliente.java
@@ -2,7 +2,6 @@ package com.delivery_api.Projeto.de.Delivery.API.entity;
 
 import java.time.LocalDateTime;
 
-import jakarta.annotation.Generated;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
